4a03d318e88c7d26d4b680c3ecff34d0b5bf24bb
fauna5 <richarddchamberlain@gmail.com>
2013-07-26 21:57:16 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,47 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function (line) {
+	var input = parseInt(line,10);
+	if (isNaN(input) || input < 1 || input > oneBillion){
+		console.log( "Must be a positive integer less than a billion");
+	}
+	console.log(toText(input));
+});
+
+var ones = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];	
+var tens = ['','ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety'];	
+var oneMillion = 1000000;
+var oneBillion = 1000000000;
+
+var toText = function(input){	
+	if (input < 19){
+		return ones[input];	
+	} else if (input < 100) {
+		var amountOfTens = Math.floor(input/10);
+		input = input - amountOfTens * 10;
+		return tens[amountOfTens] + ' ' + toText(input);
+	} else if (input < 1000) {
+		var amountOfHundreds = Math.floor(input/100);
+		input = input - amountOfHundreds * 100;
+		var hundredsText = ones[amountOfHundreds] + ' hundred';
+		var restOfText = toText(input);
+		if(restOfText.length > 0){
+			restOfText = ' and ' + restOfText;
+		}
+		return hundredsText + restOfText;
+	} else if (input < oneMillion) {
+		var amountOfThousands = Math.floor(input/1000);
+		input = input - amountOfThousands * 1000;
+		return toText(amountOfThousands) + ' thousand ' + toText(input);
+	} else if (input < oneBillion) {
+		var amountOfMillions = Math.floor(input/oneMillion);
+		input = input - amountOfMillions * oneMillion;
+		return toText(amountOfMillions) + ' million ' + toText(input);
+	}
+}
